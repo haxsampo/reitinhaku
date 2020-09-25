3f2295f45f4c11d7f088808aa185df90a9488aec
smape <sampo.hakola@helsinki.fi>
2020-09-26 01:44:23 +0300

    .

---
+++ src/main/java/tiralabra/reitinhaku/Imager.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiralabra.reitinhaku;
+
+import java.awt.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
+import javax.swing.*;
+
+
+public class Imager {
+      
+    public static void main(String[] args) throws Exception{
+    new Imager();
+  }
+        
+ public void Imager() throws Exception
+  {
+    SwingUtilities.invokeLater(new Runnable()
+    {
+      public void run()
+      {
+        JFrame editorFrame = new JFrame("Image Demo");
+        editorFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        
+        BufferedImage image = null;
+        try
+        {
+          image = ImageIO.read(new File("C:/Users/toni_/Koulu/tiralabra/bootybay.png"));
+        }
+        catch (Exception e)
+        {
+          e.printStackTrace();
+          System.exit(1);
+        }
+        ImageIcon imageIcon = new ImageIcon(image);
+        JLabel jLabel = new JLabel();
+        jLabel.setIcon(imageIcon);
+        editorFrame.getContentPane().add(jLabel, BorderLayout.CENTER);
+
+        editorFrame.pack();
+        editorFrame.setLocationRelativeTo(null);
+        editorFrame.setVisible(true);
+      }
+    });
+  }
+}
